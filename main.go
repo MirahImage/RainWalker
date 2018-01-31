2d18402a62ea0be74a23f9f486eca87174fcc5a6
Mirah Gary <mirah.gary@gmail.com>
2018-01-30 21:38:46 -0500

    Refactored to allow stubs Implement IsCovered

--- main.go
+++ main.go
@@ -12,13 +12,26 @@ const appToken = "Hl8EuekONWAZmQj7osuAIKPTg"
 
 // NumberSheds queries the NYC sheds API.
 // It returns the number of sheds and any errors.
-func NumberSheds() (uint, error) {
-	nyData := soda.NewGetRequest(shedsEndpoint, appToken)
-	return nyData.Count()
+func NumberSheds(shedReq *soda.GetRequest) (uint, error) {
+	return shedReq.Count()
+}
+
+// IsCovered queries the NYC sheds API for a specific address.
+// It returns whether there is a shed at the given address.
+func IsCovered(shedReq *soda.GetRequest, borough string, streetName string, houseNumber string) (bool, error) {
+	shedReq.Format = "json"
+	shedReq.Filters["borough"] = borough
+	shedReq.Filters["street_name"] = streetName
+	shedReq.Filters["house__"] = houseNumber
+	shedReq.Query.Limit = 10
+
+	count, err := shedReq.Count()
+	return count > 0, err
 }
 
 func main() {
-	numSheds, err := NumberSheds()
+	shedReq := soda.NewGetRequest(shedsEndpoint, appToken)
+	numSheds, err := NumberSheds(shedReq)
 	if err != nil {
 		log.Fatal(err)
 	}
